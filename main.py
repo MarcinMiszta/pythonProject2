76c58db155294b31824343eaa7b44f1b8151daf3
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-03-03 13:21:46 +0100

    update

--- main.py
+++ main.py
@@ -37,13 +37,28 @@ import sys
 # print('liczba a jest rowna {:.2f}, liczba b jest rowna {:.2f}'
 #       .format(a, b))
 
-a = input('wprowadz liczbe: ')
-print(a)
-print(type(a))
-print(a*5)
-print(type(a))
-
-sys.stdout.write('Wprowadz liczbe ')
-b = sys.stdin.readline()
-print(b)
-print(type(b))
+# a = input('wprowadz liczbe: ')
+# print(a)
+# print(type(a))
+# print(a*5)
+# print(type(a))
+#
+# sys.stdout.write('Wprowadz liczbe ')
+# b = sys.stdin.readline()
+# print(b)
+# print(type(b))
+lista = [6, 6.6, 34, 'a', 'b', [2, 3, 4], 'ab']
+print(lista)
+lista.append(67)
+print(lista)
+lista.insert(2,'c')
+print(lista)
+lista.extend([20, 21, 22])
+print(lista)
+lista.pop()
+print(lista)
+lista.remove([2, 3, 4])
+print(lista)
+del lista[1]
+print(lista)
+del lista
