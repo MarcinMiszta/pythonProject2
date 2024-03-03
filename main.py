32c99b85ba0db162f22a6c4c05bcf6e110051139
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-03-03 12:54:47 +0100

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# a = 56
+# print(a)
+# print(type(a))
+# b = 5.5
+# print(b)
+# print(type(b))
+# zmienna_tekstowa = 'wizualizacja danych'
+# print(zmienna_tekstowa)
+# print(type(zmienna_tekstowa))
+
+a = 6
+b = 3
+
+c = a + b
+print(c)
+d = a - b
+print(d)
+e = 4
+f = b // a
+print(f)
+f = a // b
+print(f)
