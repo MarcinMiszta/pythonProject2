8722527b9333dc551102e2ac281651679b08555f
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-03-03 14:04:11 +0100

    update

--- main.py
+++ main.py
@@ -48,17 +48,77 @@ import sys
 # print(b)
 # print(type(b))
 lista = [6, 6.6, 34, 'a', 'b', [2, 3, 4], 'ab']
-print(lista)
-lista.append(67)
-print(lista)
-lista.insert(2,'c')
-print(lista)
-lista.extend([20, 21, 22])
-print(lista)
-lista.pop()
-print(lista)
-lista.remove([2, 3, 4])
-print(lista)
-del lista[1]
-print(lista)
-del lista
+# print(lista)
+# lista.append(67)
+# print(lista)
+# lista.insert(2, 'c')
+# print(lista)
+# lista.extend([20, 21, 22])
+# print(lista)
+# lista.pop()
+# print(lista)
+# lista.remove([2, 3, 4])
+# print(lista)
+# del lista[1]
+# print(lista)
+# # del lista
+# lista.reverse()
+# print(lista)
+# lista.sort()
+# print(lista)
+# slownik = {'klucz': 'wartosc', 1: 2, 'a': 5, 4: 'b'}
+# print(slownik)
+# print(slownik[4])
+# slownik[6] = 45
+# print(slownik)
+# slownik.pop(1)
+# print(slownik)
+# print(slownik.keys())
+# print(slownik.values())
+# del slownik[6]
+# print(slownik)
+
+# a = 8
+# b = 8
+# if a > b:
+#     print("a is greater than b")
+# elif a < b:
+#     print("a is less than b")
+# else:
+#     print("a is equal to b")
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+#
+# if (a > b) & (c > b):
+#     print(a, c)
+# else:
+#     print(b, d)
+#
+# if(a > b) | (c > d):
+#     print(a, c)
+# else:
+#     print(b, d)
+
+# for i in range(1, 8, 2):
+#     print(i)
+# else:
+#     print('koniec petli')
+
+# for i in lista:
+#     print(i)
+
+for i in range(0, 5):
+    for j in range(0, 5):
+        result = i+j
+        print(result)
+    print('')
+
+licznik = 0
+while licznik < len(lista):
+    print(lista[licznik])
+    licznik += 1
+else:
+    print('koniec petli')
